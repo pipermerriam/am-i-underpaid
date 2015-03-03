1391dee188931439b34c8089f4fc2966617ae8f8
Piper Merriam <piper@simpleenergy.com>
2015-03-03 11:06:44 -0700

    Change email prefix

--- disparity/apps/core/emails.py
+++ disparity/apps/core/emails.py
@@ -7,4 +7,4 @@ class DisparityEmail(MarkdownEmail):
     from_email = settings.DEFAULT_FROM_EMAIL
 
     def get_subject(self):
-        return "[Disparity] {0}".format(self.subject)
+        return "[Are We Being Underpaid] {0}".format(self.subject)
